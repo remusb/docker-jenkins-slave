ef282be3589fc7e3d98845136c67b528fa1e5da6
Matt Bentley <mbentley@mbentley.net>
2016-07-28 10:46:48 -0400

    Added ability to use secrets
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 PROTOCOL="${PROTOCOL:-http}"
 JENKINS_MASTER="${1}"
 SLAVE_NAME="${2}"
+SECRET="${3:-}"
 
 # check to see if jenkins master was specified
 if [ -z "${JENKINS_MASTER}" ]
@@ -22,6 +23,16 @@ then
   exit 1
 fi
 
+# check and see if secret was provided
+if [ -z "${SECRET}" ]
+then
+  # null value for SECRET_OPT as no secret was provided
+  SECRET_OPT=""
+else
+  # set the SECRET_OPT env var with the secret provided
+  SECRET_OPT="-secret ${SECRET}"
+fi
+
 # download slave.jar if necessary
 if [ ! -f "/slave.jar" ]
 then
@@ -29,4 +40,4 @@ then
 fi
 
 # start the jenkins slave
-exec java -jar /slave.jar -jnlpUrl ${PROTOCOL}://${JENKINS_MASTER}/computer/${SLAVE_NAME}/slave-agent.jnlp
+exec java -jar /slave.jar -jnlpUrl ${PROTOCOL}://${JENKINS_MASTER}/computer/${SLAVE_NAME}/slave-agent.jnlp ${SECRET_OPT}
